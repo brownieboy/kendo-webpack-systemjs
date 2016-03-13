e36e564771cbd278ff763b613320382d5880eccc
Michael Brown <brownieboy@gmail.com>
2016-03-13 14:50:32 +1100

    Refactored to use template

--- src/js/modules/northwind-customers-table.js
+++ src/js/modules/northwind-customers-table.js
@@ -1,20 +1,27 @@
 import "kendo-ui/src/js/kendo.grid.js";
+import getTableWrapperTemplate from './templates/tablewrappertemplate.js';
 
 
 class Table {
     constructor(props) {
         this.props = props;
         this.currentGrid = this.setupGrid();
-        this.el = props.el || "#tableWrapper";
+        this.wrapperSelector = props.wrapperSelector || "#tableWrapper";
+        this.el = props.name;  // el terminology is from Backbone.
         this.setupGrid();
     }
 
     setupGrid() {
         var that = this;
-        const dataSource = that.props.getNewDataSource({ model: that.props.getModel().model, paths: that.props.getModel().paths });
-        console.log("dataSource=" + JSON.stringify(dataSource));
+        const dataSource = this.props.getNewDataSource({ model: that.props.getModel().model, paths: that.props.getModel().paths });
 
-        $(this.el).kendoGrid({
+        var templateData = getTableWrapperTemplate(this.props.getModel().name, this.props.getModel().title || "Unknown title");
+        var parsedHTML = $.parseHTML(templateData);
+
+        var $wrapper = $(this.wrapperSelector);
+        $wrapper.append(parsedHTML);
+        var $el = $wrapper.find("#"  + this.el);
+        $el.kendoGrid({
             dataSource: dataSource,
             height: 550,
             groupable: true,
